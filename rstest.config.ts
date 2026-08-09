4123b19348272300a86395322567232c6a64dd24
alevny <alevnyacow@gmail.com>
2026-08-09 13:21:47 +0400

    fix: added coverage and thresholds

--- rstest.config.ts
+++ rstest.config.ts
@@ -3,5 +3,18 @@ import { defineConfig } from '@rstest/core';
 
 export default defineConfig({
     extends: withRslibConfig(),
-    reporters: 'verbose'
+    reporters: 'verbose',
+    coverage: {
+        enabled: true,
+        include: [
+            'src/handler/handler.ts',
+            'src/REST/create-endpoint.ts',
+        ],
+        thresholds: {
+            functions: 90,
+            statements: 55,
+            branches: 40,
+            lines: 55
+        }
+    }
 });
