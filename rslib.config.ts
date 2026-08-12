fe1a75316f4e1558822f6d475b541808a82c9095
alevny <alevnyacow@gmail.com>
2026-08-12 19:56:29 +0400

    fixed build

--- rslib.config.ts
+++ rslib.config.ts
@@ -6,24 +6,10 @@ export default defineConfig({
       format: 'esm',
       syntax: ['node 22'],
       dts: true,
-      source: {
-        entry: {
-          index: './src/index.ts',
-          next: './src/REST-adapters/next-adapter.ts',
-          express: './src/REST-adapters/express-adapter.ts',
-        }
-      }
     },
     {
       format: 'cjs',
       syntax: ['node 22'],
-      source: {
-        entry: {
-            index: './src/index.ts',
-            next: './src/REST-adapters/next-adapter.ts',
-            express: './src/REST-adapters/express-adapter.ts',
-        }
-      }
     },
   ],
 });
